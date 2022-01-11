6c4941b39fe3f63b0416c335de8057a15eb63dbe
Kevin Nzeng Essimengane <kevin.nzeng@energyweb.org>
2022-01-11 11:19:05 +0100

    feat: updating staking tests

--- libs/ew-crowdfunding-contracts/proxyContract_diamond/test/StakingTest.js
+++ libs/ew-crowdfunding-contracts/proxyContract_diamond/test/StakingTest.js
@@ -18,24 +18,14 @@ describe("StakingFacet", function () {
     })
 
     it("User can stake",  async () => {
-        // await stakingFacet.functions.stake({from: "0x50956097e0814d95B827f1c58766b4245068D93C", value: '10000000000000000000'})
-        await stakingFacet.functions.stake({from: "0x50956097e0814d95B827f1c58766b4245068D93C"})
-        console.log("Staking Content", stakingFacet);
-        console.log("Contract Balance >> ", await ethers.getDefaultProvider().getBalance(diamondAddress));
+        await stakingFacet.functions.stake({from: "0x50956097e0814d95B827f1c58766b4245068D93C", value: '1000000000000000000'});
+        //TO-Do: check all staking parameters updates
     })
-    
-    it("Should return the new greeting once it's changed", async function () {
-    const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy("Hello, world!");
-    await greeter.deployed();
 
-    expect(await greeter.greet()).to.equal("Hello, world!");
-
-    const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
-
-    // wait until the transaction is mined
-    await setGreetingTx.wait();
-
-    expect(await greeter.greet()).to.equal("Hola, mundo!");
-  });
+    it("User can't stake several times",  async () => {
+        expect(
+            stakingFacet.functions.stake(
+                {from: "0x50956097e0814d95B827f1c58766b4245068D93C", value: '1000000000000000000'}
+            )).to.be.revertedWith('Already staking');
+    })
 });
