853567a93864ee5b48c2ceaaffee5f7852f61629
Kevin Nzeng Essimengane <kevin.nzeng@energyweb.org>
2022-01-13 10:09:40 +0100

    feat: settung up a simple smartContract directory

---
+++ libs/ew-crowdfunding/smart-contracts/jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  displayName: 'ew-crowdfunding-smart-contracts',
+  preset: '../../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  transform: {
+    '^.+\\.[tj]sx?$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../../coverage/libs/ew-crowdfunding/smart-contracts',
+};
---
+++ libs/ew-crowdfunding/smart-contracts/src/lib/ew-crowdfunding-smart-contracts.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable-next-line */
+export interface EwCrowdfundingSmartContractsProps {}
+
+export function EwCrowdfundingSmartContracts(props: EwCrowdfundingSmartContractsProps) {
+  return (
+    <div>
+      <h1>Welcome to EwCrowdfundingSmartContracts!</h1>
+    </div>
+  );
+}
+
+export default EwCrowdfundingSmartContracts;
