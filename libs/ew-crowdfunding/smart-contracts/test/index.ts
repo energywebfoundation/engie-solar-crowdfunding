b820441c9ae3f80c5de716735ef235f8401ad58a
Kevin Nzeng Essimengane <kevin.nzeng@energyweb.org>
2022-01-17 11:29:10 +0100

    feat: setup tests

--- libs/ew-crowdfunding/smart-contracts/test/index.ts
+++ libs/ew-crowdfunding/smart-contracts/test/index.ts
@@ -1,19 +1,49 @@
-import { expect } from "chai";
 import { ethers } from "hardhat";
+import { expect, use } from "chai";
+import { Staking } from "../ethers";
+import { BigNumber, Wallet } from "ethers";
+import { MockProvider, solidity, deployContract, loadFixture } from "ethereum-waffle";
+import StakingContract from "../artifacts/contracts/Staking.sol/Staking.json";
 
-describe("Greeter", function () {
-  it("Should return the new greeting once it's changed", async function () {
-    const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy("Hello, world!");
-    await greeter.deployed();
+use(solidity);
 
-    expect(await greeter.greet()).to.equal("Hello, world!");
+describe("Staking", function () {
 
-    const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
+  async function fixture(
+    start: number,
+    [owner, patron]: Wallet[],
+    provider: MockProvider,
+  ) {
+    const duration = 3600 * 24 * 30;
+    const end = start + duration;
 
-    // wait until the transaction is mined
-    await setGreetingTx.wait();
+    const stakingContract = (await deployContract(owner, StakingContract)) as Staking;
 
-    expect(await greeter.greet()).to.equal("Hola, mundo!");
+
+    return {
+      stakingContract,
+      patron,
+      owner,
+      asPatron: stakingContract.connect(patron),
+      asOwner: stakingContract.connect(owner),
+      provider,
+      duration,
+      start,
+      end,
+    };
+  }
+
+  async function defaultFixture(wallets: Wallet[], provider: MockProvider) {
+    const { timestamp } = await provider.getBlock("latest");
+    const start = timestamp + 10;
+
+    return fixture(start, wallets, provider);
+  }
+  it("fails when non owner tries to initialize",  async () => {
+    const { start, owner, asPatron } = await loadFixture(
+      defaultFixture,
+    );
+
+    await expect(asPatron.init(start)).to.be.revertedWith('Must be the admin');
   });
 });
