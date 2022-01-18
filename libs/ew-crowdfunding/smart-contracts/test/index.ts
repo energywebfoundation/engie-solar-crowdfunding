8a85da46336855bb4188492c910607d85a7e8185
Kevin Nzeng Essimengane <kevin.nzeng@energyweb.org>
2022-01-18 13:27:24 +0100

    feat: setting dateHandler to set end and start dates

--- libs/ew-crowdfunding/smart-contracts/test/index.ts
+++ libs/ew-crowdfunding/smart-contracts/test/index.ts
@@ -1,9 +1,11 @@
 import { ethers } from "hardhat";
 import { expect, use } from "chai";
 import { Staking } from "../ethers";
-import { BigNumber, Wallet } from "ethers";
+import { Wallet } from "ethers";
 import { MockProvider, solidity, deployContract, loadFixture } from "ethereum-waffle";
 import StakingContract from "../artifacts/contracts/Staking.sol/Staking.json";
+import { DateHandler } from '../scripts/utils/dateUtils';
+import { ST } from "next/dist/shared/lib/utils";
 
 use(solidity);
 
@@ -14,8 +16,8 @@ describe("Staking", function () {
     [owner, patron]: Wallet[],
     provider: MockProvider,
   ) {
-    const duration = 3600 * 24 * 30;
-    const end = start + duration;
+    //set endDate 1 year ahead
+    const end = Number(DateHandler().add(1, 'years', new Date(start * 1000)));
 
     const stakingContract = (await deployContract(owner, StakingContract)) as Staking;
 
@@ -27,23 +29,47 @@ describe("Staking", function () {
       asPatron: stakingContract.connect(patron),
       asOwner: stakingContract.connect(owner),
       provider,
-      duration,
       start,
       end,
     };
   }
 
   async function defaultFixture(wallets: Wallet[], provider: MockProvider) {
-    const { timestamp } = await provider.getBlock("latest");
-    const start = timestamp + 10;
+    const dateHandler = DateHandler();
+
+    const start = (dateHandler.add(14, 'days')) as number
 
     return fixture(start, wallets, provider);
   }
+
   it("fails when non owner tries to initialize",  async () => {
-    const { start, asPatron } = await loadFixture(
+    const { start, end, asPatron } = await loadFixture(
       defaultFixture,
     );
 
-    await expect(asPatron.init(start)).to.be.revertedWith('Must be the admin');
+    await expect(asPatron.init(start, end)).to.be.revertedWith('Must be the admin');
+  });
+
+  it("fails if start time is not 2 weeks ahead initialization",  async () => {
+    const { asOwner, end } = await loadFixture(
+      defaultFixture,
+    );
+    const start = await DateHandler().now();
+
+    await expect(asOwner.init(start, end)).to.be.revertedWith('Start date should be at least 2 weeks ahead');
+  });
+
+  it("Can set start time 2 weeks ahead initialization date",  async () => {
+    const { asOwner, start, end, stakingContract, provider } = await loadFixture(
+      defaultFixture,
+    );
+    console.log("Start Date :: ", start)
+    console.log("End Date :: ", end)
+
+    const tx = await asOwner.init(start, end);
+    const { blockNumber } = await tx.wait();
+        const { timestamp } = await provider.getBlock(blockNumber);
+
+    await expect(tx).to.emit(stakingContract, 'StakingPoolInitialized').withArgs(timestamp, start, end);
   });
 });
