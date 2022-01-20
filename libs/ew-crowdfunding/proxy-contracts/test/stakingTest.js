3173ac35f89f2ee9c6814f54e4f05fd6a5896d74
Kevin Nzeng Essimengane <kevin.nzeng@energyweb.org>
2022-01-20 03:17:21 +0100

    feat: adding tests to check limits

--- libs/ew-crowdfunding/proxy-contracts/test/stakingTest.js
+++ libs/ew-crowdfunding/proxy-contracts/test/stakingTest.js
@@ -11,6 +11,7 @@ const DateHandler = require('../scripts/dateUtils.js');
 use(solidity);
 
 describe("StakingFacet", function () {
+    const oneEWT = ethers.utils.parseUnits("1", "ether");
 
     async function fixture(
         start,
@@ -18,14 +19,19 @@ describe("StakingFacet", function () {
         provider
       ) {
         //set endDate 1 year ahead
+        
         const end = Number(DateHandler().add(1, 'years', new Date(start * 1000)));
-    
+        const hardCap = oneEWT.mul(1000000);
+        const contributionLimit = oneEWT.mul(10);
+
         return {
             end,
             owner,
             start,
             patron,
+            hardCap,
             provider,
+            contributionLimit,
         };
     }
 
@@ -51,32 +57,61 @@ describe("StakingFacet", function () {
         end = params.end;
         start = params.start;
         patron = params.patron;
+        hardCap = params.hardCap;
         provider = params.provider;
         asOwner = stakingFacet.connect(params.owner);
+        contributionLimit = params.contributionLimit;
         asPatron = stakingFacet.connect(params.patron);
     })
 
     it("fails when non owner tries to initialize",  async () => {
-        await expect(asPatron.init(start, end)).to.be.revertedWith('LibDiamond: Must be contract owner');
+        await expect(asPatron.init(
+            start,
+            end,
+            hardCap,
+            contributionLimit
+        )).to.be.revertedWith('LibDiamond: Must be contract owner');
     });
 
     it("fails if start time is not 2 weeks ahead initialization",  async () => {
         const wrongStart = await DateHandler().now();
-        await expect(asOwner.init(wrongStart, end)).to.be.revertedWith('Start date should be at least 2 weeks ahead');
+        await expect(asOwner.init(
+            wrongStart, 
+            end,
+            hardCap,
+            contributionLimit
+        )).to.be.revertedWith('Start date should be at least 2 weeks ahead');
     });
 
     it('fails when staking on non initialized contract', async () => {
         await expect(asPatron.stake({value: 10000})).to.be.revertedWith('Not initialized');
     });
 
+    it('fails when initialize with contributionLimit higher than hardCap', async () => {
+        const wrongContributionLimit = oneEWT.mul(100000);
+        const wrongHardCap = oneEWT.mul(200);
+
+        await expect(asOwner.init(
+            start,
+            end,
+            wrongHardCap,
+            wrongContributionLimit
+            )).to.be.revertedWith('hardCap exceeded');
+    })
+
     it("Can set start time 2 weeks ahead initialization date",  async () => {
    
-    console.log("Start Date :: ", start)
-    console.log("End Date :: ", end)
+        console.log("Start Date :: ", start)
+        console.log("End Date :: ", end)
 
-    const tx = await asOwner.init(start, end);
-    const { blockNumber } = await tx.wait();
-    const { timestamp } = await provider.getBlock(blockNumber);
+        const tx = await asOwner.init(
+            start,
+            end,
+            hardCap,
+            contributionLimit
+        );
+        const { blockNumber } = await tx.wait();
+        const { timestamp } = await provider.getBlock(blockNumber);
 
     await expect(tx).to.emit(stakingFacet, 'StakingPoolInitialized').withArgs(timestamp, start, end);
     });
@@ -85,12 +120,19 @@ describe("StakingFacet", function () {
        await expect(asPatron.unstake()).to.be.revertedWith('No Ewt at stake');
     });
 
+    it('fails when staking more thant limit', async () => {
+        await expect(asPatron.stake({
+                value: contributionLimit + 42
+            }),
+        ).to.be.revertedWith('Stake greater than contribution limit');
+    })
+
     it("Can stake on initialized contract",  async () => {
         await expect(
             await asPatron.stake({
-                value: 10000
+                value: 1
             }),
-        ).changeEtherBalance(diamond, 10000);
+        ).changeEtherBalance(diamond, 1);
     })
 
     // it("User can't stake several times",  async () => {
