50a7cece3d03e972a93216b27cca5889deccea77
Florin Georgescu <flo_georgescu@hotmail.com>
2022-01-20 13:36:26 +0200

    Started working on Lending details

---
+++ apps/ew-crowdfunding/containers/LendingDetails/LendingDetails.styles.ts
@@ -0,0 +1,9 @@
+import { makeStyles } from '@mui/styles';
+
+export const useStyles = makeStyles(() => {
+  return {
+    wrapper: {
+   
+    }
+  };
+});
---
+++ apps/ew-crowdfunding/containers/LendingDetails/LendingDetails.tsx
@@ -0,0 +1,9 @@
+/* eslint-disable @next/next/no-img-element */
+import { Box, Typography } from '@mui/material';
+import { FC } from 'react';
+import { useStyles } from './LendingDetails.styles';
+
+export const LendingDetails: FC = () => {
+  const classes = useStyles();
+  return <Box>Lending details</Box>;
+};
