ed8cbcacf52d6aa11ff2d59f7ad28db7f96a56df
Kevin Nzeng Essimengane <kevin.nzeng@energyweb.org>
2022-01-21 14:42:59 +0100

    feat: adding substraction function on dateHandler

--- libs/ew-crowdfunding/smart-contracts/scripts/utils/dateUtils.ts
+++ libs/ew-crowdfunding/smart-contracts/scripts/utils/dateUtils.ts
@@ -3,7 +3,7 @@ const date = require('date-and-time');
 export const DateHandler = () => {
     let dateTimestamp;
     const defaultDate = new Date(Date.now() + 10000);
-    const add = (amount: Number, type: string, baseDate = defaultDate) => {
+    const add = (amount: number, type: string, baseDate = defaultDate) => {
         switch (type){
             case 'months':
                 dateTimestamp = Date.parse(date.addMonths(baseDate, amount));
@@ -33,13 +33,43 @@ export const DateHandler = () => {
         }
     }
 
-    const now = async () => {
-        const currentDate : Date = new Date(Date.now());
+    const sub = (amount : number, type : string, baseDate = defaultDate) => {
+        switch (type){
+            case 'months':
+                dateTimestamp = Date.parse(date.addMonths(baseDate, -amount));
+                return dateTimestamp / 1000;
+            case 'days':
+                dateTimestamp = Date.parse(date.addDays(baseDate, -amount));
+                return dateTimestamp / 1000;
+            case 'years':
+                dateTimestamp = Date.parse(date.addYears(baseDate, -amount));
 
-        return (Date.parse(currentDate.toDateString()) / 1000);
+                return dateTimestamp / 1000;
+            case 'hours':
+                 dateTimestamp = Date.parse(date.addHours(baseDate, -amount));
+                return dateTimestamp / 1000;
+            case 'minutes':
+                dateTimestamp = Date.parse(date.addMinutes(baseDate, -amount));
+                return dateTimestamp / 1000;
+            case 'seconds':
+                dateTimestamp = Date.parse(date.addSeconds(baseDate, -amount));
+                return dateTimestamp / 1000;
+            case 'milliseconds':
+                dateTimestamp = Date.parse(date.addMilliseconds(baseDate, -amount));
+                return dateTimestamp / 1000;
+            default:
+                return new Error(`Substraction is not possible on ${type}`);
+        }
+    }
+
+    const now = async () => {
+        const currentDate = date.format( new Date(), 'YYYY/MM/DD HH:mm:ss');
+
+        return (Date.parse(currentDate) / 1000);
     }
     return {
         add,
+        sub,
         now
     }
 }
