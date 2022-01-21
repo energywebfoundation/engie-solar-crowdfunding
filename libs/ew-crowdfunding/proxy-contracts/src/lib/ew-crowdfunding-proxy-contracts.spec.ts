49e4032f713e15168dabb718d482b16f78116ef1
Kevin Nzeng Essimengane <kevin.nzeng@energyweb.org>
2022-01-21 16:49:23 +0100

    build: remove proxy-contracts

--- libs/ew-crowdfunding/proxy-contracts/src/lib/ew-crowdfunding-proxy-contracts.spec.ts
+++
@@ -1,7 +0,0 @@
-import { ewCrowdfundingProxyContracts } from './ew-crowdfunding-proxy-contracts';
-
-describe('ewCrowdfundingProxyContracts', () => {
-  it('should work', () => {
-    expect(ewCrowdfundingProxyContracts()).toEqual('ew-crowdfunding-proxy-contracts');
-  });
-});
