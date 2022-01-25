f2fed81dad16589ee51a82800ac6a586bf8b31cb
Kevin Nzeng <kevin.nzeng@energyweb.org>
2022-01-25 13:09:20 +0100

    Feat/ds 154/stake before start (#13)
    * feat: adding init function
    
    * fix: checking deposit before returning
    
    * fix: checking staking status before removingStaker
    
    * fix: checking staking status before withdrawal
    
    * feat: init contract and setting deposit and withdraw
    
    * fix: fixing weeks duration on init function to set end date to 1 year after start
    
    * feat: enable JSON file resolution
    
    * feat: setup tests
    
    * chore: removing example test
    
    * chore: cleaning unused variable
    
    * fix: setting endDate manually
    
    * fix: update endDate param on event
    
    * feat: setting dateHandler to set end and start dates
    
    * feat: teaking time to handle block delay in block.timestamp
    
    * cleanup
    
    * fix: defining event in stakingFacet and add 10 seconds delays to signup date
    
    * fix: removing ganache network
    
    * feat: handling date-and-time
    
    * fix: fixing testing environment to remove ganache dependency
    
    * feat: rejects staking on uninitialized contract
    
    * feat: testing initialized modifier
    
    * feat: prevent staking on uninitialized contract
    
    * chore: update test label
    
    * chore: diamond tests
    
    * feat: setting and checking hardCap and limit
    
    * feat: adding tests to check limits
    
    * chore: correcting typos in tests labels
    
    * feat: adding tests on initialization limits
    
    * feat: adding subtraction on dates and fixes now function
    
    * fix: update unstaking condiion
    
    * feat: testing staking on time limits
    
    * feat: adding substraction function on dateHandler
    
    * feat: adding getNextDay function to dateHandler
    
    * feat: setting signup period
    
    * feat: adding tests for signup period
    
    * build: remove proxy-contracts

--- libs/ew-crowdfunding/smart-contracts/test/index.ts
+++ libs/ew-crowdfunding/smart-contracts/test/index.ts
@@ -3,7 +3,7 @@ import { Staking } from "../ethers";
 import { MockProvider, solidity, deployContract, loadFixture } from "ethereum-waffle";
 import StakingContract from "../artifacts/contracts/Staking.sol/Staking.json";
 import { DateHandler } from '../scripts/utils/dateUtils';
-import { Wallet, utils, BigNumber } from "ethers";
+import { Wallet, utils, BigNumber, ContractTransaction } from "ethers";
 
 use(solidity);
 
@@ -11,24 +11,56 @@ let end : number;
 let start : number;
 let asOwner : Staking;
 let asPatron : Staking;
+let asPatron2: Staking;
+let signupEnd : number;
 let hardCap : BigNumber;
+let signupStart : number;
 let provider : MockProvider;
-let stakingContract : Staking;
 let contributionLimit : BigNumber;
+
+const timeTravel = async (provider: MockProvider, seconds: number) => {
+  await provider.send("evm_increaseTime", [seconds]);
+  await provider.send("evm_mine", []);
+};
+
+const initializeContract = async (
+    contract : Staking,
+    start : number,
+    end : number,
+    hardCap : BigNumber,
+    contributionLimit : BigNumber,
+    signupStart : number,
+    signupEnd : number
+  ) : Promise<ContractTransaction> => {
+   const transaction =  await contract.init(
+    signupStart,
+    signupEnd,
+    start,
+    end,
+    hardCap,
+    contributionLimit
+    );
+
+    return transaction;
+}
+
+const dateHandler = DateHandler();
 
 describe("Staking", function () {
   const oneEWT = utils.parseUnits("1", "ether");
 
   async function fixture(
     start: number,
-    [owner, patron]: Wallet[],
+    [owner, patron, patron2]: Wallet[],
     provider: MockProvider,
   ) {
     //set endDate 1 year ahead
-    const end = Number(DateHandler().add(1, 'years', new Date(start * 1000)));
+    const end = Number(dateHandler.add(1, 'years', new Date(start * 1000)));
     const hardCap = oneEWT.mul(1000000);
     const contributionLimit = oneEWT.mul(10);
     const stakingContract = (await deployContract(owner, StakingContract)) as Staking;
+    signupStart = await dateHandler.now();
+    signupEnd = Number(dateHandler.sub(1, 'days', new Date(start * 1000))); //Signup ends 1 day before startDate
 
 
     return {
@@ -38,15 +70,18 @@ describe("Staking", function () {
       patron,
       provider,
       hardCap,
+      signupEnd,
+      signupStart,
       stakingContract,
       contributionLimit,
       asOwner: stakingContract.connect(owner),
-      asPatron: stakingContract.connect(patron)
+      asPatron: stakingContract.connect(patron),
+      asPatron2: stakingContract.connect(patron2)
     };
   }
 
   async function defaultFixture(wallets: Wallet[], provider: MockProvider) {
-    const dateHandler = DateHandler();
+    // const dateHandler = DateHandler();
 
     const start = (dateHandler.add(14, 'days')) as number
 
@@ -60,29 +95,148 @@ describe("Staking", function () {
         hardCap = params.hardCap;
         asOwner = params.asOwner;
         asPatron = params.asPatron;
+        provider = params.provider;
+        asPatron2 = params.asPatron2;
+        signupEnd = params.signupEnd;
+        signupStart = params.signupStart;
         contributionLimit = params.contributionLimit;
   })
 
   it("fails when non owner tries to initialize",  async () => {
 
-    await expect(asPatron.init(
+     
+    await expect(initializeContract(
+      asPatron,
       start,
       end,
       hardCap,
-      contributionLimit
-    )).to.be.revertedWith('Must be the admin');
+      contributionLimit,
+      signupStart,
+      signupEnd
+      )
+    ).to.be.revertedWith('Must be the admin');
   });
 
-  it("fails if start time is not 2 weeks ahead initialization",  async () => {
+  it("fails if startDate is set before signup period",  async () => {
   
-    const wrongStart = await DateHandler().now();
+    let wrongStart = signupStart;
 
-    await expect(asOwner.init(
-      wrongStart,
+    //Checking initialization failure on each day during signup period
+    while (wrongStart <= signupEnd){
+      await expect(initializeContract(
+        asOwner,
+        wrongStart,
+        end,
+        hardCap,
+        contributionLimit,
+        signupStart,
+        signupEnd
+      )).to.be.revertedWith('Start febore signup period');
+      wrongStart = dateHandler.getNextDay(new Date(wrongStart * 1000))
+    }
+  });
+
+  it('fails if signup period has wrong configuration', async () => {
+    await expect(initializeContract(
+      asOwner,
+      start,
       end,
       hardCap,
+      contributionLimit,
+      signupEnd, // Inversion of signup End and signup start (end < start)
+      signupStart,
+    )).to.be.revertedWith('Wrong signup config');
+
+    await expect(initializeContract(
+      asOwner,
+      start,
+      end,
+      hardCap,
+      contributionLimit,
+      signupStart,
+      signupStart, // Setting the same date on signup End and signup start
+    )).to.be.revertedWith('Wrong signup config');
+  });
+
+  it('fails when staking on non initialized contract', async () => {
+    await expect(asPatron.stake(
+      {value: 10000}
+    )).to.be.revertedWith('Not initialized');
+  });
+
+  it('fails when contract is initialized with contributionLimit higher than hardCap', async () => {
+    const wrongHardCap = oneEWT.mul(200);
+    const wrongContributionLimit = oneEWT.mul(100000);
+  
+    await expect(initializeContract(
+      asOwner,
+      start,
+      end,
+      wrongHardCap,
+      wrongContributionLimit,
+      signupStart,
+      signupEnd
+    )).to.be.revertedWith('hardCap exceeded');
+  })
+
+  it("Can set start time 2 weeks ahead initialization date",  async () => {
+    const {
+      end,
+      start,
+      asOwner,
+      hardCap,
+      provider,
+      stakingContract,
       contributionLimit
-    )).to.be.revertedWith('Start date should be at least 2 weeks ahead');
+    } = await loadFixture(
+      defaultFixture,
+    );
+    console.log("Start Date :: ", start)
+    console.log("End Date :: ", end)
+
+    const tx = await initializeContract(asOwner, start, end, hardCap, contributionLimit, signupStart, signupEnd);
+    const { blockNumber } = await tx.wait();
+        const { timestamp } = await provider.getBlock(blockNumber);
+
+    await expect(tx).to.emit(stakingContract, 'StakingPoolInitialized').withArgs(timestamp, start, end);
+  });
+
+  it('fails when staking more than limit', async () => {
+    await initializeContract(asOwner, start, end, hardCap, contributionLimit, signupStart, signupEnd);
+   
+    await expect(asPatron.stake({
+            value: contributionLimit.add(BigNumber.from(42))
+        }),
+    ).to.be.revertedWith('Stake greater than contribution limit');
+  });
+
+  it("Can stake before startDate on initialized contract",  async () => {
+    await initializeContract(asOwner, start, end, hardCap, contributionLimit, signupStart, signupEnd);
+
+    await expect(
+        await asPatron.stake({
+            value: 1
+        }),
+    ).changeEtherBalance(asPatron, 1);
+  });
+
+  it("fails if patron stakes more than once", async () => {
+    await expect(asPatron.stake({value: 1})).to.be.revertedWith('Already staking');
+  });
+
+  it('fails when trying to stake after startDate', async () => {
+    const tx = await initializeContract(asOwner, start, end, hardCap, contributionLimit, signupStart, signupEnd);
+
+    const { blockNumber } = await tx.wait();
+    const { timestamp } = await provider.getBlock(blockNumber);
+    const afterStart = start - timestamp + 42000;
+    await timeTravel(provider, afterStart);
+
+    await expect(asPatron2.stake(
+      {
+        value: 1
+      }),
+    ).to.be.revertedWith('Staking contributions are no longer accepted');
   });
 
   it('fails when staking on non initialized contract', async () => {
