6339a97685790b2d61c7707621a14af5dc218839
Kevin Nzeng Essimengane <kevin.nzeng@energyweb.org>
2022-01-26 13:45:48 +0100

    chore: removing dupplication of ERC20 import

--- libs/ew-crowdfunding/smart-contracts/contracts/Staking.sol
+++ libs/ew-crowdfunding/smart-contracts/contracts/Staking.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 import './StakingBase.sol';
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
 contract Staking is StakingBase, ERC20Burnable {
