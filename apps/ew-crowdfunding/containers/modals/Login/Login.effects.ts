19b3f92e1e6201aec5cf72efe6fcf914bfb7cc8b
Florin Georgescu <flo_georgescu@hotmail.com>
2022-02-01 16:00:35 +0200

    Improved metamask connection

--- apps/ew-crowdfunding/containers/modals/Login/Login.effects.ts
+++ apps/ew-crowdfunding/containers/modals/Login/Login.effects.ts
@@ -1,10 +1,13 @@
 import { DSLAModalsActionsEnum, useDSLAModalsDispatch, useDSLAModalsStore } from '../../../context';
+import detectMetamask from '@metamask/detect-provider';
+import { useEffect, useState } from 'react';
 
 export const useLoginEffects = () => {
   const {
     login: { open, isConnectedToRightNetwork, isMetamaskPresent, login },
   } = useDSLAModalsStore();
   const dispatchModals = useDSLAModalsDispatch();
+  const [noMetamaskInstalled, setNoMetamaskInstalled] = useState(true);
 
   const closeModal = () => {
     dispatchModals({
@@ -18,11 +21,57 @@ export const useLoginEffects = () => {
     });
   };
 
+  const importMetamaskConf = async () => {
+    try {
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      const metamaskProvider: any = await detectMetamask({
+        mustBeMetaMask: true,
+      });
+
+      if (!metamaskProvider) {
+        throw new Error('MetaMask not detected');
+      }
+
+      await metamaskProvider.request({
+        method: 'wallet_addEthereumChain',
+        params: [
+          {
+            chainId: `0x${Number(process.env.NEXT_PUBLIC_CHAIN_ID).toString(16)}`, // Hexadecimal version of chain ID
+            chainName: process.env.NEXT_PUBLIC_NETWORK_NAME,
+            nativeCurrency: {
+              name: process.env.NEXT_PUBLIC_CURRENCY_NAME,
+              symbol: process.env.NEXT_PUBLIC_CURRENCY_SYMBOL,
+              decimals: 18,
+            },
+            rpcUrls: [process.env.NEXT_PUBLIC_RPC_URL],
+            blockExplorerUrls: [process.env.NEXT_PUBLIC_BLOCK_EXPLORER_URL],
+            iconUrls: [''],
+          },
+        ],
+      });
+      window.location.reload();
+    } catch (addError) {
+      console.log('Did not add network');
+    }
+  };
+
+  const handleInstall = () => {
+    window.location.replace("https://metamask.io/");
+  };
+  
+  useEffect(() => {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    setNoMetamaskInstalled(!((window as any).web3 || (window as any).ethereum));
+  }, []);
+
   return {
     open,
     closeModal,
     isConnectedToRightNetwork,
     isMetamaskPresent,
     login,
+    importMetamaskConf,
+    handleInstall,
+    noMetamaskInstalled,
   };
 };
