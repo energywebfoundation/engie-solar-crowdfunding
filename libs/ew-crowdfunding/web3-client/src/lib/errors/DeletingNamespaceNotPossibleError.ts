bb40657a0d09a806a79f2c3b05ae44f677683002
Florin Georgescu <flo_georgescu@hotmail.com>
2022-02-18 12:57:46 +0200

    Removed local iam-client-lib

--- libs/ew-crowdfunding/web3-client/src/lib/errors/DeletingNamespaceNotPossibleError.ts
+++
@@ -1,9 +0,0 @@
-export class DeletingNamespaceNotPossibleError extends Error {
-    constructor({ namespace, notOwnedNamespaces }: { namespace: string; notOwnedNamespaces: string[] }) {
-        super(
-            `Deleting ${namespace} not possible because you're not owner of ${notOwnedNamespaces.join(", ")} ${
-                notOwnedNamespaces.length > 1 ? "namespaces" : "namespace"
-            }`,
-        );
-    }
-}
--- libs/ew-crowdfunding/web3-client/src/lib/signer/index.ts
+++
@@ -1,5 +0,0 @@
-export * from "./signer.service";
-export * from "./signer.types";
-export * from "./privateKeySigner";
-export * from "./metamaskSigner";
-export * from "./walletConnectMetamask";
