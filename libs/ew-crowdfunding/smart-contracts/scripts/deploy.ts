b60e050129ececb420e89739c5de4097cd8cb339
Kevin Nzeng Essimengane <kevin.nzeng@energyweb.org>
2022-02-22 02:16:07 +0100

    feat: setting deployment script

--- libs/ew-crowdfunding/smart-contracts/scripts/deploy.ts
+++ libs/ew-crowdfunding/smart-contracts/scripts/deploy.ts
@@ -1,30 +1,79 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// When running the script with `npx hardhat run <script>` you'll find the Hardhat
-// Runtime Environment's members available in the global scope.
-import { ethers } from "hardhat";
+// import { Contract } from "ethers";
+const Contract = require("ethers").Contract;
+const Staking = require("../ethers").Staking;
+// import { Staking } from "../ethers";
 
-async function main() {
-  // Hardhat always runs the compile task when running scripts with its command
-  // line interface.
-  //
-  // If this script is run directly using `node` you may want to call compile
-  // manually to make sure everything is compiled
-  // await hre.run('compile');
+const _prompt = require("prompt-sync")();
 
-  // We get the contract to deploy
-  const Greeter = await ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+const emoji = require("node-emoji");
+const { ethers } = require("hardhat");
 
-  await greeter.deployed();
+const displayContractInfos = (_contractName:string, _contract : typeof Staking) => {
+  console.log(`\n[ ${_contractName}'s infos ]`);
+  console.log(`\tAddress: ${_contract.address}\n`);
+};
 
-  console.log("Greeter deployed to:", greeter.address);
+const checkAnswer = (answer : string, promptMode = "noLoop") => {
+  const isInvalid = (answer : string) => (answer !== "n" && answer !== "N" && answer !== "Y" && answer !== "y");
+    if (promptMode === "loop"){
+      while (isInvalid(answer)){
+        console.log(`\t${emoji.emojify(":rotating_light:")} Invalid option \" ${answer}\" ... Please choose a valid option !`);
+        answer = _prompt("Init? (Y/n) ") as string;
+      }
+    } else {
+      if (isInvalid(answer)){
+        console.log(`\t${emoji.emojify(":x:")} \"${answer}\" is not a valid option. Aborting ...`);
+      }
+    }
+    if (answer !== "Y" && answer != "y") {
+        process.exit(0);
+    }
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+const getClaimManagerAddress = (hardhatNetwork : string | undefined) => {
+  const EWC_CLAIM_MANAGER_ADDRESS = "0x23b026631A6f265d17CFee8aa6ced1B244f3920C";
+  const VOLTA_CLAIM_MANAGER_ADDRESS = "0xC3dD7ED75779b33F5Cfb709E0aB02b71fbFA3210";
+
+  return hardhatNetwork === 'ewc' ? EWC_CLAIM_MANAGER_ADDRESS : VOLTA_CLAIM_MANAGER_ADDRESS;
+}
+
+const deployContract = async (contractName : string) => {
+  const answer = _prompt(`\t${emoji.emojify(":warning:")}\tYou are about to deploy ${contractName} contract to ${process.env.HARDHAT_NETWORK} network. This will consume some EWT. Do you Confirm ? (Y/n) :  `) as string;
+
+  checkAnswer(answer);
+  console.log(`\t${emoji.emojify(":hourglass_flowing_sand:")} Deploying ${contractName} ...`);
+  
+  const Contract = await ethers.getContractFactory(contractName);
+
+  const claimManagerAddress = getClaimManagerAddress(process.env.HARDHAT_NETWORK);
+
+  try {
+    const deployedContract = await Contract.deploy(
+      claimManagerAddress,
+      process.env.SERVICE_ROLE,
+      process.env.PATRON_ROLE,
+      process.env.TOKEN_NAME,
+      process.env.TOKEN_SYMBOL
+    );
+    displayContractInfos(contractName, deployedContract);
+    console.log(`${emoji.emojify(":large_green_circle:")} ${contractName} deployed ${emoji.emojify(":rocket:")}`);
+
+    return deployedContract;
+  } catch (error) {
+    console.log(`${emoji.emojify(":red_circle:")} An error occurred during contract deployment ${error}`);
+    return undefined;
+  }
+};
+
+const deploy = async () => {  
+    const stakingPoolContract = await deployContract("Staking");
+    //Find a wait to export contract address, i.e deployedContract.address
+    return stakingPoolContract.address
+}
+
+deploy()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
