49a91fa4e34332bbc105666f13b1053c76419436
Kevin Nzeng Essimengane <kevin.nzeng@energyweb.org>
2022-03-02 10:39:14 +0100

    feat: exporting artifacts

--- libs/ew-crowdfunding/smart-contracts/src/index.ts
+++ libs/ew-crowdfunding/smart-contracts/src/index.ts
@@ -1,2 +1,4 @@
+export * from '../ethers/Staking';
 export * from './lib/deployedAddress';
+export * from '../ethers/factories/Staking__factory';
 export * from './lib/ew-crowdfunding-smart-contracts';
