6a8d72e10e939ee7c958cb0d8e061ad1ce56b578
Florin Georgescu <flo_georgescu@hotmail.com>
2022-03-04 19:24:37 +0200

    Fixed lending stats type

--- apps/ew-crowdfunding/containers/LendingStats/LendingStats.effects.ts
+++ apps/ew-crowdfunding/containers/LendingStats/LendingStats.effects.ts
@@ -12,7 +12,7 @@ export const useLendingStatsEffects = () => {
     dispatch(getTotalLentAmount(provider));
   }, [dispatch, provider]);
 
-  const totalLentAmount = `${Number(useSelector(selectTotalLentAmount))?.toPrecision(3)}`;
+  const totalLentAmount = Number(useSelector(selectTotalLentAmount))?.toPrecision(3);
   const globalTokenLimit = useSelector(selectGlobalTokenLimit);
 
   useEffect(() => {
