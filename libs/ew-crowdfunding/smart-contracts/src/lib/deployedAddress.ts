f0295d29d917d6ecaba2e3bc780d6f8fbb9ec205
Kevin Nzeng Essimengane <kevin.nzeng@energyweb.org>
2022-03-31 03:16:24 +0200

    feat: adding a function to fetch contract owner

--- libs/ew-crowdfunding/smart-contracts/src/lib/deployedAddress.ts
+++ libs/ew-crowdfunding/smart-contracts/src/lib/deployedAddress.ts
@@ -1,3 +1,4 @@
 // export const deployedAddress = '0xC7FD9226Cd7d8d66145950B3D5d58C02B316397d';
-export const deployedAddress = '0x3198959F65Bcf35b85d076FbdB52A3E02D71b99B';
+// export const deployedAddress = '0x3198959F65Bcf35b85d076FbdB52A3E02D71b99B';
+export const deployedAddress = '0x1C0CC71a7766d39a6243CD1A2eFB7FA6771c7461';
 //export const deployedAddress = '0xdcD75179E292ef2d3d435A3aceEf73C7a0612E83';
