c3b93bf9814fe257af9150cc3be2fa4d4192f332
Kevin Nzeng Essimengane <kevin.nzeng@energyweb.org>
2022-04-11 14:34:12 +0200

    chore: update campaign status messages

--- apps/ew-crowdfunding/utils/StakingTimelineEnum.ts
+++ apps/ew-crowdfunding/utils/StakingTimelineEnum.ts
@@ -1,8 +1,8 @@
 export enum StakingTimelineEnum {
-  BEFORE_STAKING = 'Staking not activated yet',
-  ACTIVATE_STAKING = 'Activate staking',
-  CLOSE_STAKING = 'Close staking',
-  LOCK_STAKES = 'Lock stakes',
-  RELEASE_REWARDS = 'Release rewards',
-  FINAL_STOP = 'Campaign is complete',
+  BEFORE_STAKING = 'Staking has not started yet.',
+  ACTIVATE_STAKING = 'Staking is open.',
+  CLOSE_STAKING = 'Staking is close.',
+  LOCK_STAKES = 'Stakes are locked.',
+  RELEASE_REWARDS = 'Rewards are available.',
+  FINAL_STOP = 'Campaign is complete.',
 }
